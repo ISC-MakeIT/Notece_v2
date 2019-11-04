9fdfb773bda3042f9b53f518362518475a5c600a
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-11-04 20:44:38 +0900

    add Canvas FC

---
+++ client/src/App/Canvas/Canvas.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+// import DrawStage from "module";
+// import controlerStage from "module";
+
+const Canvas: FC = () => {
+    return (
+        <div>
+            {/* DrawStage */}
+            {/* controlerStage */}
+        </div>
+    );
+};
+
+export default Canvas;
