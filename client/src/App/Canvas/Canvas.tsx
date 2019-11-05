8032da94205fa8f7ed166baa0236a3784189b229
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-11-05 09:42:09 +0900

    ADD rect,circle,text,image,line State

--- client/src/App/Canvas/Canvas.tsx
+++ client/src/App/Canvas/Canvas.tsx
@@ -1,16 +1,37 @@
-
-import React, { FC } from 'react';
-// import DrawStage from "module";
-// import controlerStage from "module";
+import React, { FC, useState } from 'react';
+import DrawStage from './components/DrawStage';
+// import controlerStage from './MenuButton';
 
 const Canvas: FC = () => {
+    // [図形のデータ（大きさや位置）、更新用]
+    const [rect, setRect] = useState([]);
+    const [circle, setcircle] = useState([]);
+    const [text, setText] = useState([]);
+    const [image, setImage] = useState([]);
+    const [line, setLine] = useState([]);
+
+    // undo,redo用のState
+    const [shape, setShape] = useState([]);
+
+    // 図形生成用関数（controlerStageの引数）
+    const addRect = () => {};
+    const addCircle = () => {};
+    const addText = () => {};
+    const addImage = () => {};
+    const addLine = () => {};
+
     return (
         <div>
-            {/* DrawStage */}
+            <DrawStage
+                rects={rect}
+                circles={circle}
+                texts={text}
+                images={image}
+                lines={line}
+            />
             {/* controlerStage */}
         </div>
     );
 };
 
 export default Canvas;
-
