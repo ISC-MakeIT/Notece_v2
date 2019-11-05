de3c96fe15c169aa9d9f678f15a49f093dccef6c
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-11-05 10:45:14 +0900

    ADD react-konva

--- client/src/App/Canvas/components/DrawStage.tsx
+++ client/src/App/Canvas/components/DrawStage.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 // import konva from 'konva';
-// import { Stage } from 'react-konva';
+import { Stage, Layer, Rect } from 'react-konva';
 
 // anyで仮置
 interface DrawStageProps {
@@ -25,27 +25,25 @@ const DrawStage: FC<DrawStageProps> = ({
     // zoom outするためのイベント
 
     return (
-        <>
-            {/* <Stage width={} height={}> */}
-            {/* <Layer> */}
-            {/* {rects.map((rect, i) => {
-                return;
-            })}
-            {circles.map((circle, i) => {
-                return;
-            })}
-            {texts.map((text, i) => {
-                return;
-            })}
-            {images.map((image, i) => {
-                return;
-            })}
-            {lines.map((line, i) => {
-                return;
-            })} */}
-            {/* </Layer> */}
-            {/* </Stage> */}
-        </>
+        <Stage width={window.innerWidth} height={window.innerHeight}>
+            <Layer>
+                {/* {rects.map((rect, i) => {
+                    return;
+                })}
+                {circles.map((circle, i) => {
+                    return;
+                })}
+                {texts.map((text, i) => {
+                    return;
+                })}
+                {images.map((image, i) => {
+                    return;
+                })}
+                {lines.map((line, i) => {
+                    return;
+                })} */}
+            </Layer>
+        </Stage>
     );
 };
 
