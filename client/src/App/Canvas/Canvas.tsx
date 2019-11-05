a9d9e53399216e6651d776a625dd7550eaac447d
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-11-05 14:02:13 +0900

    ADD addRect,Circle,Text,Image,Line function

--- client/src/App/Canvas/Canvas.tsx
+++ client/src/App/Canvas/Canvas.tsx
@@ -4,18 +4,41 @@ import DrawStage from './components/DrawStage';
 
 const Canvas: FC = () => {
     // [図形のデータ（大きさや位置）、更新用]
-    const [rect, setRect] = useState([]);
-    const [circle, setcircle] = useState([]);
-    const [text, setText] = useState([]);
-    const [image, setImage] = useState([]);
-    const [line, setLine] = useState([]);
+    const initArray: Array<JSON> = [];
+    const [rect, setRect] = useState(initArray);
+    const [circle, setcircle] = useState(initArray);
+    const [text, setText] = useState(initArray);
+    const [image, setImage] = useState(initArray);
+    const [line, setLine] = useState(initArray);
 
     // undo,redo用のState
-    const [shape, setShape] = useState([]);
+    const [shape, setShape] = useState(initArray);
 
     // 図形生成用関数（controlerStageの引数）
-    const addRect = () => {};
-    const addCircle = () => {};
+    const addRect = () => {
+        const newRect: any = {
+            x: 100,
+            y: 100,
+            width: 100,
+            height: 100,
+            fill: 'red',
+            id: `rect${rect.length + 1}`
+        };
+        const tmp = rect.concat([newRect]);
+        setRect(tmp);
+    };
+    const addCircle = () => {
+        const newRect: any = {
+            x: 100,
+            y: 100,
+            width: 100,
+            height: 100,
+            fill: 'red',
+            id: `rect${rect.length + 1}`
+        };
+        const tmp = rect.concat([newRect]);
+        setRect(tmp);
+    };
     const addText = () => {};
     const addImage = () => {};
     const addLine = () => {};
