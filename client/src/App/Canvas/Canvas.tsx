341a865f0b461fe9f1ef1e0aedb808c7b656b9f8
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-11-06 17:01:18 +0900

    ADD Stage Layer  Rect  Circle

--- client/src/App/Canvas/Canvas.tsx
+++ client/src/App/Canvas/Canvas.tsx
@@ -1,6 +1,11 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useRef, createRef } from 'react';
 import DrawStage from './components/DrawStage';
+import { Stage, Layer, Image } from 'react-konva';
+import Circle from './components/Circle';
+import Rect from './components/Rect';
+import { addListener } from 'cluster';
 // import controlerStage from './MenuButton';
+import { addLine } from './components/line';
 
 const Canvas: FC = () => {
     // [図形のデータ（大きさや位置）、更新用]
@@ -11,8 +16,14 @@ const Canvas: FC = () => {
     const [image, setImage] = useState(initArray);
     const [line, setLine] = useState(initArray);
 
+    const stageEl: any = createRef();
+    const layerEl: any = createRef();
+
     // undo,redo用のState
     const [shape, setShape] = useState(initArray);
+
+    // 図形選択用State
+    const [selectedId, selectShape] = React.useState(null);
 
     // 図形生成用関数（controlerStageの引数）
     const addRect = () => {
@@ -27,6 +38,7 @@ const Canvas: FC = () => {
         const tmp = rect.concat([newRect]);
         setRect(tmp);
     };
+
     const addCircle = () => {
         const newCircle: any = {
             x: 100,
@@ -34,26 +46,72 @@ const Canvas: FC = () => {
             width: 100,
             height: 100,
             fill: 'red',
-            id: `rect${rect.length + 1}`
+            id: `circle${circle.length + 1}`
         };
         const tmp = circle.concat([newCircle]);
         setCircle(tmp);
     };
+
     const addText = () => {};
     const addImage = () => {};
-    const addLine = () => {};
+    const drawLine = () => {
+        addLine(stageEl.current.getStage(), layerEl.current);
+    };
 
     return (
-        <div>
-            <DrawStage
-                rects={rect}
-                circles={circle}
-                texts={text}
-                images={image}
-                lines={line}
-            />
-            {/* controlerStage */}
-        </div>
+        <>
+            <Stage
+                width={window.innerWidth}
+                height={window.innerHeight}
+                ref={stageEl}
+                onMouseDown={e => {
+                    // deselect when clicked on empty area
+                    const clickedOnEmpty = e.target === e.target.getStage();
+                    if (clickedOnEmpty) {
+                        selectShape(null);
+                    }
+                }}
+            >
+                <Layer ref={layerEl}>
+                    {rect.map((rects: any, i: any) => {
+                        return (
+                            <Rect
+                                key={i}
+                                shapeProps={rects}
+                                isSelected={rects.id === selectedId}
+                                onSelect={() => {
+                                    selectShape(rects.id);
+                                }}
+                                onChange={(newAttrs: any) => {
+                                    const tmp = rect.slice();
+                                    tmp[i] = newAttrs;
+                                    setRect(tmp);
+                                }}
+                            />
+                        );
+                    })}
+                    {circle.map((circles: any, i: number) => {
+                        return (
+                            <Circle
+                                key={i}
+                                shapeProps={circles}
+                                isSelected={circles.id === selectedId}
+                                onSelect={() => {
+                                    selectShape(circles.id);
+                                }}
+                                onChange={(newAttrs: any) => {
+                                    const tmp = circle.slice();
+                                    tmp[i] = newAttrs;
+                                    setCircle(tmp);
+                                }}
+                            />
+                        );
+                    })}
+                </Layer>
+            </Stage>
+            <button onClick={addCircle}>Circle</button>
+            <button onClick={addRect}>Rect</button>
+        </>
     );
 };
 
