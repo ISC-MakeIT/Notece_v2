6f0f19659ff56e50a1dffb8c1c189607b359d30c
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-11-07 10:29:13 +0900

    ADD Rectangle FC

--- client/src/App/Canvas/components/Rect.tsx
+++ client/src/App/Canvas/components/Rect.tsx
@@ -1,8 +1,60 @@
-import React, { FC } from 'react';
-import { Rect } from 'react-konva';
+import React, { FC, useRef, useEffect } from 'react';
+import { Rect, Transformer } from 'react-konva';
 
-const Rectangle: FC = () => {
-    return <></>;
+interface RectProps {
+    shapeProps: any;
+    isSelected: any;
+    onSelect: any;
+    onChange: any;
+}
+
+const Rectangle: FC<RectProps> = ({
+    shapeProps,
+    isSelected,
+    onSelect,
+    onChange
+}) => {
+    const shapeRef = React.useRef();
+    const trRef: any = React.useRef();
+    React.useEffect(() => {
+        if (isSelected) {
+            trRef.current.setNode(shapeRef.current);
+            trRef.current.getLayer().batchDraw();
+        }
+    }, [isSelected]);
+
+    return (
+        <React.Fragment>
+            <Rect
+                onClick={onSelect}
+                ref={shapeRef}
+                {...shapeProps}
+                draggable
+                onDragEnd={e => {
+                    onChange({
+                        ...shapeProps,
+                        x: e.target.x(),
+                        y: e.target.y()
+                    });
+                }}
+                onTransformEnd={e => {
+                    // transformer is changing scale
+                    const node: any = shapeRef.current;
+                    const scaleX = node.scaleX();
+                    const scaleY = node.scaleY();
+                    node.scaleX(1);
+                    node.scaleY(1);
+                    onChange({
+                        ...shapeProps,
+                        x: node.x(),
+                        y: node.y(),
+                        width: node.width() * scaleX,
+                        height: node.height() * scaleY
+                    });
+                }}
+            />
+            {isSelected && <Transformer ref={trRef} />}
+        </React.Fragment>
+    );
 };
-
-export default Rectangle;
+export default Circ;
