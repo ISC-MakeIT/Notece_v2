7b7f196fc933825e9d8d7be5fe14297f35307948
56miyano <bjgm1290561@gn.iwasaki.ac.jp>
2019-11-13 16:03:28 +0900

    add SideNav.tsx nav id=SideNav

--- client/src/App/Main/Nav/SideNav.tsx
+++ client/src/App/Main/Nav/SideNav.tsx
@@ -3,14 +3,13 @@ import React from 'react';
 const SideNav: React.FC = () => {
 
   return (
-    <section id = "SideNav">
+    <nav id ="SideNav">
       <div id="newnote-tag"></div>
       <div id="file-tag"></div>
       <div id="profile-tag"></div>
       <div id="setting-tag"></div>
-    </section>
+    </nav>
   );
-
 }
 
 export default SideNav;
