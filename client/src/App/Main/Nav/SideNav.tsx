340cec14c72bd8f146cb6842036d195ea954ecae
56miyano <bjgm1290561@gn.iwasaki.ac.jp>
2019-11-14 09:45:29 +0900

    add SideNav.tsx Navlist style[]

--- client/src/App/Main/Nav/SideNav.tsx
+++ client/src/App/Main/Nav/SideNav.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import Tag from './Component/Tag'
 
 const SideNav: React.FC = () => {
+  const Navlist: any[] = [
+    {
+      id: 'newnote-tag',
+      src: 'img/redTag.png',
+    },
+    {
+      id: 'file-tag',
+      src: 'img/yellowTag.png',
+    },
+    {
+      id: 'file-tag',
+      src: 'img/blueTag.png',
+    },
+    {
+      id: 'profile-tag',
+      src: 'img/orangeTag.png',
+    },
+  ]
 
   return (
-    <nav id ="SideNav">
-      <div id="newnote-tag"></div>
-      <div id="file-tag"></div>
-      <div id="profile-tag"></div>
-      <div id="setting-tag"></div>
+    <nav id="SideNav">
+      <Tag Navlist={Navlist} />
     </nav>
   );
 }
