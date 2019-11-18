5140d2260a2a864bdb83b702fa801e58a7411d74
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-11-18 12:38:43 +0900

    :new:ADD TimeLine,NewNote,Profile,Settings.tsx
    ~

---
+++ client/src/App/Main/Contents/NewNote.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const NewNote: FC = () => {
+    return <></>;
+};
+
+export default NewNote;
---
+++ client/src/App/Main/Contents/TimeLine.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const TimeLine: FC = () => {
+    return <></>;
+};
+
+export default TimeLine;
