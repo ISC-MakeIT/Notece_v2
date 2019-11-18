3212f256ae7ab4a419717198e1364df43b36f4db
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-11-18 11:41:56 +0900

    :sparkles:ADD Email Form
    ~

---
+++ client/src/App/RegistrationPage/contents/SignUpEmail.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react';
+import {} from 'react-router';
+import {} from 'react-router-dom';
+
+const SignUpEmail: FC = () => {
+    return (
+        <section>
+            <h1>メールアドレス登録</h1>
+            <h2>1.メールアドレス</h2>
+            <p>登録するメールアドレスを入力してください</p>
+            <input type="email" id="" />
+            <button>パスワード設定へ！</button>
+        </section>
+    );
+};
+
+export default SignUpEmail;
