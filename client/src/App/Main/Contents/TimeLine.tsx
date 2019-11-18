8e83d81fac0ebcdd815cbf729758e8c779d63139
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-11-18 21:40:44 +0900

    :books:ADD necessarily item explanation
    ~

--- client/src/App/Main/Contents/TimeLine.tsx
+++ client/src/App/Main/Contents/TimeLine.tsx
@@ -1,7 +1,23 @@
 import React, { FC } from 'react';
+import styled from 'styled-components';
+import {} from 'styled-components-ts';
 
 const TimeLine: FC = () => {
-    return <></>;
+    return (
+        <Wrapper>
+            {/*
+             * 作品のタイトル
+             * 作成者(アイコン)
+             * 作者のコメント
+             * 日時
+             */}
+        </Wrapper>
+    );
 };
+
+const Wrapper = styled.div`
+    height: 100%;
+    width: 100%;
+`;
 
 export default TimeLine;
