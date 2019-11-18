6cc4e0ad2e709875661b01fc4b7fa9b564a431dc
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-11-18 22:29:39 +0900

    :sparkles:ADD Center tag
    ~

--- client/src/App/RegistrationPage/contents/SignUpEmail.tsx
+++ client/src/App/RegistrationPage/contents/SignUpEmail.tsx
@@ -1,17 +1,26 @@
 import React, { FC } from 'react';
 import {} from 'react-router';
 import {} from 'react-router-dom';
+import styled from 'styled-components';
 
 const SignUpEmail: FC = () => {
     return (
-        <section>
+        <Center>
             <h1>メールアドレス登録</h1>
             <h2>1.メールアドレス</h2>
             <p>登録するメールアドレスを入力してください</p>
-            <input type="email" id="" />
-            <button>パスワード設定へ！</button>
-        </section>
+            <form action="/sign_up_pass">
+                <input type="email" id="" />
+                <button>パスワード設定へ！</button>
+            </form>
+        </Center>
     );
 };
+
+const Center = styled.section`
+    width: 100%;
+    height: 100%;
+    text-align: center;
+`;
 
 export default SignUpEmail;
