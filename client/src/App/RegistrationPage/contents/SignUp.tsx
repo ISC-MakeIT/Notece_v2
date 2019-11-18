11f46acbe700685aa6ed9ec9f2bd32ab44f7a218
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-11-18 10:21:40 +0900

    :sparkles:ADD Move SignUpEmail Page Link
    ~

--- client/src/App/RegistrationPage/contents/SignUp.tsx
+++ client/src/App/RegistrationPage/contents/SignUp.tsx
@@ -1,7 +1,13 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 const SingUp: FC = () => {
-    return <></>;
+    return (
+        <div>
+            <h1>新規作成</h1>
+            <Link to={'/sign_up_email'}>ここから登録</Link>
+        </div>
+    );
 };
 
 export default SingUp;
