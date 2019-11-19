9f8ea8639a43720581bd4c5bc8006368e71ab41e
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-11-19 11:20:03 +0900

    :art:ADD style to TagNav.tsx
    ~

---
+++ client/src/App/shared/TagNav.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const TagNav = styled.div`
+    height: 98%;
+    position: absolute;
+    right: 0.625%;
+    top: 1%;
+    width: 13.75%;
+`;
+
+export default TagNav;
