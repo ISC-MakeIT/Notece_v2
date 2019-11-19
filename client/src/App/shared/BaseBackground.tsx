f607bdc9bcf6bbec7b4b06c120f66571acd2a0ed
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-11-19 10:35:12 +0900

    :art:ADD style to BaseBackground.tsx
    ~

---
+++ client/src/App/shared/BaseBackground.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const BaseBackground = styled.div`
+background: #C49A6A;
+border-radius: 24px;
+height: 100%;
+width: 100%:
+`;
+
+export default BaseBackground;
