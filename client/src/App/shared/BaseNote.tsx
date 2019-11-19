bce856c1529f62b59330eba1a8bc99ced994be40
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-11-19 10:42:12 +0900

    :art:ADD style to BaseNote.tsx
    ~

---
+++ client/src/App/shared/BaseNote.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const BaseNote = styled.div`
+    background: #fff;
+    height: 98%;
+    position: absolute;
+    left: 0;
+    top: 1%;
+    width: 91.25%;
+`;
+
+export default BaseNote;
