26f5a373bca15effa54878928c6263efb92aca82
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-11-19 09:40:25 +0900

    :truck:CHANGE SignUpEmail.tsx :point_right: SignUpInfo.tsx
    ~

---
+++ client/src/App/RegistrationPage/contents/SignUpInfo.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from 'react';
+import {} from 'react-router';
+import {} from 'react-router-dom';
+import styled from 'styled-components';
+// import Tags from '../../shared/Tags';
+// import BaseNote from '../../shared/BaseNote';
+
+const SignUpEmail: FC = () => {
+    return (
+        // <BaseBackground>
+        //     <TagNav>
+        //         <Tags />
+        //         <Tags />
+        //         <Tags />
+        //         <Tags />
+        //     </TagNav>
+        //     <BaseNote></BaseNote>
+        // </BaseBackground>
+    );
+};
+
+const Center = styled.section`
+    width: 100%;
+    height: 100%;
+    text-align: center;
+`;
+
+export default SignUpEmail;
