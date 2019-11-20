def3ef101795357776082da87bdb0a502c2501cd
56miyano <bjgm1290561@gn.iwasaki.ac.jp>
2019-11-20 15:57:34 +0900

    add SideNav.tsx import img

--- client/src/App/Main/Nav/SideNav.tsx
+++ client/src/App/Main/Nav/SideNav.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
 import Tag from './Component/Tag'
+import red from './Component/img/redTag.png';
+import blue from './Component/img/blueTag.png';
+import orange from './Component/img/orangeTag.png';
+import yellow from './Component/img/yellowTag.png';
 
 const SideNav: React.FC = () => {
   const Navlist: any[] = [
     {
       id: 'newnote-tag',
-      src: 'img/redTag.png',
+      src: 'img.src={red}',
     },
     {
       id: 'file-tag',
-      src: 'img/yellowTag.png',
+      src: 'img.src={yellow}',
     },
     {
       id: 'file-tag',
-      src: 'img/blueTag.png',
+      src: 'img.src={blue}',
     },
     {
       id: 'profile-tag',
-      src: 'img/orangeTag.png',
+      src: 'img.src={orange}',
     },
   ]
 
