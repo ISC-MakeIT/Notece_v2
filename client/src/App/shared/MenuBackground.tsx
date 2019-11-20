bdc6e5e0fb921a47b55c39ec7b6e4a66d9546cbb
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-11-20 13:11:02 +0900

    :new:ADD Content,MenuBackground,MenuContent.tsx
    ~

---
+++ client/src/App/shared/MenuBackground.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const MenuBackground = styled.div`
+    box-sizing: border-box;
+    background: #c49a6a;
+    display: grid;
+    grid-template-rows: 100%;
+    grid-template-columns: 25% 75%;
+    height: 100vh;
+    padding: 2.5%;
+    width: 100%;
+`;
+
+export default MenuBackground;
