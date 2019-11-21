38c2dbdf7b29d55c6e3b42e0112f8d251fcc627a
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-11-21 10:48:13 +0900

    :recycle:UPDATE import path
    ~

--- client/src/App/RegistrationPage/RegistrationPage.tsx
+++ client/src/App/RegistrationPage/RegistrationPage.tsx
@@ -2,14 +2,16 @@ import React, { FC } from 'react';
 import { Switch, Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import SignUp from './contents/SignUp';
-import SignUpInfo from './contents/SignUpInfo';
+import SignUpEmail from './contents/SignUpEmail';
+import SignUpPass from './contents/SignUpPass';
 
 const RegistrationPage: FC = () => {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/sign_up" component={SignUp} />
-                <Route path="/sign_up_info" component={SignUpInfo} />
+                <Route path="/sign_up_email" component={SignUpEmail} />
+                <Route path="/sign_up_pass" component={SignUpPass} />
                 <Route path="/sign_in" />
             </Switch>
         </BrowserRouter>
