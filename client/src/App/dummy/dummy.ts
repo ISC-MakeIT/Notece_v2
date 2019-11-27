d318034f92b489db9c1901e4e00d39f674862a2d
isshun226 <bjsi1290346@gn.iwasaki.ac.jp>
2019-11-28 01:01:22 +0900

    [WiP]Added dummy data for testing

---
+++ client/src/App/dummy/dummy.ts
@@ -0,0 +1,26 @@
+export const dummy = [
+    {
+        title: 'JavaScript',
+        posted_at: '2019/11/27 19:2:31',
+        image: '',
+        note: ''
+    },
+    {
+        title: 'Python',
+        posted_at: '2019/11/27 19:2:31',
+        image: '',
+        note: ''
+    },
+    {
+        title: 'Java',
+        posted_at: '2019/11/27 19:2:31',
+        image: '',
+        note: ''
+    },
+    {
+        title: 'PHP',
+        posted_at: '2019/11/27 19:2:31',
+        image: '',
+        note: ''
+    }
+];
