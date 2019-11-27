9dcdfc3f61417bc6f246857a316ecdf288a4228b
isshun226 <bjsi1290346@gn.iwasaki.ac.jp>
2019-11-27 14:57:14 +0900

    remove Grid

--- client/src/App/Main/Main.tsx
+++ client/src/App/Main/Main.tsx
@@ -1,16 +1,7 @@
 import React, { FC } from 'react';
 
-import { Grid } from '@material-ui/core';
-
 import { TimeLine } from './TimeLine/TimeLine';
 
 export const Main: FC = () => {
-    return (
-        <Grid container>
-            <Grid item xs={3}></Grid>
-            <Grid item xs={9}>
-                <TimeLine />
-            </Grid>
-        </Grid>
-    );
+    return <TimeLine />;
 };
