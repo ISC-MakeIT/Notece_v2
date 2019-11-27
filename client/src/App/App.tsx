c37928cec606a43e4052150690dea8169189d075
isshun226 <bjsi1290346@gn.iwasaki.ac.jp>
2019-11-28 00:59:40 +0900

    Added router for main and canvas

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,11 +1,19 @@
 import React, { FC } from 'react';
+import { Switch, Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
+import { RegistrationPage } from './RegistrationPage/RegistrationPage';
 import { Main } from './Main/Main';
+import { Canvas } from './Canvas/Canvas';
 
 export const App: FC = () => {
     return (
-        <div>
-            <Main />
-        </div>
+        <BrowserRouter>
+            <Switch>
+                <Route path="/registration" component={RegistrationPage} />
+                <Route path="/top" component={Main} />
+                <Route path="/canvas" component={Canvas} />
+            </Switch>
+        </BrowserRouter>
     );
 };
